8add5b8ec3d3d17e44989c5bca8c959078e31e98
Zaver85 <Zaver85.com@mail.ru>
2024-12-04 13:24:02 +0700

    created J_J_hw1

--- src/AverageEvenNumbers.java
+++ src/AverageEvenNumbers.java
@@ -3,13 +3,11 @@ import java.util.List;
 
 public class AverageEvenNumbers {
     public static void main(String[] args) {
-        // Исходный список чисел
+
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        // Вычисление среднего значения четных чисел
         double average = calculateAverageOfEvenNumbers(numbers);
 
-        // Вывод результата
         System.out.println("Среднее значение четных чисел: " + average);
     }
 
